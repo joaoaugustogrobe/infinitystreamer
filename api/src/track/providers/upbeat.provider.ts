f2a30e035a27a70513c2f71448ed99311ec62ff2
John <joaocastilho@alunos.utfpr.edu.br>
2024-06-21 13:21:14 -0300

    Update upbeat provider, old endpoint were offline

--- api/src/track/providers/upbeat.provider.ts
+++ api/src/track/providers/upbeat.provider.ts
@@ -6,19 +6,19 @@ import { NotFoundException } from '@nestjs/common';
 export class UpbeatProvider implements TrackProvider {
   async getTrends(): Promise<Track[]> {
     const endpoint =
-      'https://fastly.uppbeat.io/api/tracks/all/types/1/ids/0/energies/0/vocals/Trending/sorts/0/durations/[]/stylefilters/-1/limits/[]/excludes/0/premium/0/freeonly/1';
+      'https://fastly-y.uppbeat.io/api/admin/trending/view-more/top-downloads/tracks/free/0';
     try {
       const response = await axios.get(endpoint);
-      const data = response.data;
+      const data = response.data?.top_downloads_tracks_free;
 
-      const tracks: Track[] = data.results.map((item: any) => ({
-        id: `upbeat:${item.artist.slug}:${item.slug}`,
+      const tracks: Track[] = data.map((item: any) => ({
+        id: `upbeat:${item.contributor.slug}:${item.slug}`,
         title: item.name,
-        duration: item.track_url.version_length,
-        thumbnail: item.artist.avatar_image.image_aws_url,
-        resourceUrl: item.track_url.version_preview_uri,
+        duration: item.asset_specific_data.length,
+        thumbnail: item.image,
+        resourceUrl: item.asset_specific_data.preview_url,
         timelineStartAt: -1,
-        artistName: item.artist.name,
+        artistName: item.contributor.name,
         resourceOrigin: 'upbeat',
       }));
       return tracks;
@@ -28,10 +28,8 @@ export class UpbeatProvider implements TrackProvider {
   }
 
   async getNewTrackById(id: string): Promise<Track | null> {
-    console.log('id', id);
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [_trackProviderName, artist, trackId] = id.split(':');
-    const endpoint = `https://fastly.uppbeat.io/api/tracks/${artist}/artists/${trackId}/track`;
+    const endpoint = `https://fastly-y.uppbeat.io/api/tracks/${artist}/artists/${trackId}/track`;
 
     try {
       const response = await axios.get(endpoint);
@@ -43,14 +41,14 @@ export class UpbeatProvider implements TrackProvider {
       const track: Track = {
         id: `upbeat:${item.artist.slug}:${item.slug}`,
         title: item.name,
-        duration: item.track_url.version_length,
+        duration: item.track_version[0].version_length,
         thumbnail: item.artist.avatar_image.image_aws_url,
-        resourceUrl: item.track_url.version_preview_uri,
+        resourceUrl: item.track_version[0].version_preview_uri,
         timelineStartAt: -1,
         artistName: item.artist.name,
         resourceOrigin: 'upbeat',
         timeline: null,
-        waveform: item.track_url.waveform_json,
+        waveform: item.track_version[0].waveform_json,
       };
       return track;
     } catch (error) {
